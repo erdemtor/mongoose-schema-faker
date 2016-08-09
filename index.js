730374ac005f7dd62695ffbaf91abf7646cae110
erdemtoraman <erdemtoraman93@gmail.com>
2016-08-09 22:40:21 +0300

    big steps, requires lots of enhancements though

--- index.js
+++ index.js
@@ -3,22 +3,31 @@
  */
 const functionMap = {
     "Array" : createArray,
-    "String": createString
+    "String": createString,
+    "Number": createNumber
 };
 const illegal_keys = ["_id","__v"];
-let schema = require("./model/university").schema.paths;
+let schema = require("./model/event").schema.paths;
 let _ = require("lodash");
 
 
-createObject(schema);
+console.log(createObject(schema));
 
-
+function createNumber(){
+    return Math.round(Math.random()*150);
+}
 function createString(specs){
     return randomString();
 }
 function createArray(specs) {
     if (specs.$isMongooseDocumentArray){ // if specs has a schema then it means in the array there are objects
-
+        let createdArray = [];
+        let randomNumber = Math.round(Math.random()*10)+1;
+        for(let i = 0; i < randomNumber; i++){
+            let outputValue = createObject(specs.schema.paths);;
+            createdArray.push(outputValue);
+        }
+        return createdArray;
     }
     else{
         let createdArray = [];
@@ -31,15 +40,24 @@ function createArray(specs) {
     }
 }
 function createObject(schema){
+    let outputObject ={};
     let keys = Object.keys(schema);
     _.each(keys, function (key) {
         if(illegal_keys.indexOf(key) >= 0) return;
         let specs = schema[key];
-        console.log(key);
-        if(functionMap[specs.instance])
-            console.log(functionMap[specs.instance](specs));
-
+        if(functionMap[specs.instance]) {
+            let value = checkEnumValues(specs) || functionMap[specs.instance](specs);
+            outputObject[key] = value;
+        }
     });
+    return JSON.parse(JSON.stringify(outputObject));
+}
+function  checkEnumValues(specs) {
+    if(specs.enumValues && specs.enumValues.length > 0){
+        let randomIndex =Math.floor((Math.random() * specs.enumValues.length));
+        return specs.enumValues[randomIndex];
+    }
+    return undefined;
 }
 
 function randomString()
