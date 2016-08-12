5130426b0b47e29b8d96600045fd4efe744d6d5b
erdemtoraman <erdemtoraman93@gmail.com>
2016-08-12 22:53:09 +0300

    database connection and objectifying

--- index.js
+++ index.js
@@ -2,8 +2,22 @@
  * Created by erdem on 8.08.16.
  */
 const Factory = require("./lib/factory");
-let schema = require("./model/university").schema.paths;
-console.log(Factory.createObject(schema));
+const fs = require("fs");
+const Async = require("async");
+let model = require("./model/tag");
+let schema = model.schema.paths;
+const callback = function (err, res) {
+    console.log(err + " "+ res);
+};
+Async.auto({
+    "connect" : function(acb){
+        require("./lib/floppy")(require("./config/config").mongooseUrl,acb)
+    },
+    "create": ["connect", function (result ,acb) {
+       let factory = new Factory(model);
+        factory.createObject(schema,100000,acb);
+    }]
+},callback);
 
 
 
